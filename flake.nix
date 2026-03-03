5d06c6fadab3107cf6a245c3a7b20052649e4c9e
kaede-0323 <ocho.study@gmail.com>
2026-03-04 00:28:37 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "NDLOCR-lite NixOS flake modules";
 
   inputs = {
-    nixpkgs.follows = "nixpkgs";
     pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
     ndlocr-src.url = "github:ndl-lab/ndlocr-lite";
     ndlocr-src.flake = false;
