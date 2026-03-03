8b98d57e7421cc2fc5d5004f4ecf7a4c43b941b6
kaede-0323 <ocho.study@gmail.com>
2026-03-04 00:25:44 +0900

    fix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,16 @@
   description = "NDLOCR-lite NixOS flake modules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.nixpkgs.follows = "nixpkgs";
     pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
     ndlocr-src.url = "github:ndl-lab/ndlocr-lite";
     ndlocr-src.flake = false;
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: {
-    nixosModules.ndlocrLite = import ./modules/nixos.nix;
+  outputs = {self, ...} @ inputs: {
+    nixosModules.ndlocrLite = {pkgs, ...}:
+      import ./modules/nixos.nix {
+        inherit pkgs inputs;
+      };
   };
 }
