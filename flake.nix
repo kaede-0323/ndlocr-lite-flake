8cc32f1587817fbbfeaf5a6d3b01180d8acd59d5
kaede-0323 <ocho.study@gmail.com>
2026-03-04 01:12:54 +0900

    aaaaa

--- flake.nix
+++ flake.nix
@@ -1,16 +1,45 @@
 {
-  description = "NDLOCR-lite NixOS flake modules";
+  description = "ndlocr-lite CLI";
 
-  inputs = {
-    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-    ndlocr-src.url = "github:ndl-lab/ndlocr-lite";
-    ndlocr-src.flake = false;
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {self, ...} @ inputs: {
-    nixosModules.ndlocrLite = {pkgs, ...}:
-      import ./modules/nixos.nix {
-        inherit pkgs inputs;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ndl-lab";
+      repo = "ndlocr-lite";
+      rev = "master";
+      hash = "sha256-7LgCxm+kx6+iVRLl7EIIaaWpSbodPzVgqMmrm2Kd8iI=";
+    };
+    python = pkgs.python312.withPackages (ps:
+      with ps; [
+        dill
+        lxml
+        networkx
+        onnxruntime
+        pillow
+        ordered-set
+        protobuf
+        pyparsing
+        pyyaml
+        tqdm
+        reportlab
+        pypdfium2
+        numpy
+        opencv-python-headless
+      ]);
+  in {
+    packages.${system}.default = pkgs.writeShellApplication {
+      name = "ndlocr-lite-cli";
+      runtimeInputs = [python];
+      text = ''
+        exec python ${src}/src/ocr.py "$@"
+      '';
+    };
   };
 }
